515e8ddf05f5393f04ad3420d6482a2b4f0c3e2c
Adrian Janicki <adrijan93@gmail.com>
2017-07-06 14:00:32 +0200

    Added images and buttons

--- workshop/src/AppBundle/Menu/Builder.php
+++ workshop/src/AppBundle/Menu/Builder.php
@@ -14,11 +14,17 @@ class Builder implements ContainerAwareInterface
     {
         $menu = $factory->createItem('root');
 
+        $menu->setChildrenAttribute('class', 'nav nav-pills');
+
         $menu->addChild('Home', array('route' => 'homepage'));
-        $menu->addChild('Categories', array('route' => 'category_index'));
-        $menu->addChild('Register', array('route' => 'fos_user_registration_register'));
-        $menu->addChild('Login', array('route' => 'fos_user_security_login'));
-        
+
+        if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $menu->addChild('My Profile', array('route' => 'fos_user_profile_show'));
+            $menu->addChild('Logout', array('route' => 'fos_user_security_logout'));
+        } else {
+
+        }
+
         return $menu;
 
     }
